1b229bc73c52a7bb48aa6dce51fd8c220f513afd
haeju050 <haeju050@gmail.com>
2024-04-20 22:57:44 +0900

    연습용 올리기

---
+++ test004.py
@@ -0,0 +1,13 @@
+for\
+i\
+in\
+range(1, 15, 3) :
+    print(\
+        i,\
+              i*i,\
+                  i*\
+                    i*i,\
+                      sep = "|",\
+                                  end = "\n\n"\
+                                    )
+    
